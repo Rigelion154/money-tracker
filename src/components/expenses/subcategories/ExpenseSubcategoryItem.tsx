914fad5c185293408c75430c8d4816caa423b318
Илья Кузнецов <crmprog3@mtkrosberg.ru>
2025-10-09 14:53:03 +0300

    test: without acc item

--- src/components/expenses/subcategories/ExpenseSubcategoryItem.tsx
+++ src/components/expenses/subcategories/ExpenseSubcategoryItem.tsx
@@ -33,36 +33,34 @@ const ExpenseSubcategoryItem = ({ subcategory, category }: ISubcategoryItemProps
       children: <ExpenseDetailsModal id={id} />,
     });
 
-  if (subcategory.expenses.length < 5) {
+  if (subcategory.expenses.length < 1) {
     return null;
   }
 
   return (
     <Accordion className="px-0" key={subcategory.id}>
-      <Accordion.Item eventKey={subcategory.id}>
-        <Accordion.Button
-          className="rounded-0 py-1 px-2 shadow-none text-white"
-          style={{ backgroundColor: category.color }}
-        >
-          <div className="col-6">{subcategory.title}</div>
-          <div className="col-2 text-center">{percentage}%</div>
-          <div className="col-4 text-end">{currencyString}</div>
-        </Accordion.Button>
+      <Accordion.Button
+        className="rounded-0 py-1 px-2 shadow-none text-white"
+        style={{ backgroundColor: category.color }}
+      >
+        <div className="col-6">{subcategory.title}</div>
+        <div className="col-2 text-center">{percentage}%</div>
+        <div className="col-4 text-end">{currencyString}</div>
+      </Accordion.Button>
 
-        <Accordion.Body className="p-1 border bg-light">
-          {subcategory.expenses.map((expense) => (
-            <div
-              className={styles.expense__list_wrapper}
-              key={expense.id + expense.subcategory_id}
-              id={expense.id + expense.subcategory_id}
-              onClick={() => handleExpenseClick(expense.id)}
-            >
-              <div className="">{moment(expense.date).format(FULL_MONTH_FORMAT)}</div>
-              <div className=" text-end">{getCurrencyString(expense.amount)}</div>
-            </div>
-          ))}
-        </Accordion.Body>
-      </Accordion.Item>
+      <Accordion.Body className="p-1 border bg-light">
+        {subcategory.expenses.map((expense) => (
+          <div
+            className={styles.expense__list_wrapper}
+            key={expense.id + expense.subcategory_id}
+            id={expense.id + expense.subcategory_id}
+            onClick={() => handleExpenseClick(expense.id)}
+          >
+            <div className="">{moment(expense.date).format(FULL_MONTH_FORMAT)}</div>
+            <div className=" text-end">{getCurrencyString(expense.amount)}</div>
+          </div>
+        ))}
+      </Accordion.Body>
     </Accordion>
   );
 };

7b54eb693f42903b7cbf0b83eca12fd97ed00740
Илья Кузнецов <crmprog3@mtkrosberg.ru>
2025-10-09 09:09:07 +0300

    fix: test memo burger

--- src/components/layuot/BurgerMenu.tsx
+++ src/components/layuot/BurgerMenu.tsx
@@ -1,17 +1,18 @@
 import type { Dispatch, SetStateAction } from 'react';
 import { Offcanvas } from 'react-bootstrap';
+import { observer } from 'mobx-react-lite';
 
 interface IBurgerMenuProps {
   visible: boolean;
   setVisible: Dispatch<SetStateAction<boolean>>;
 }
 
-const BurgerMenu = ({ visible, setVisible }: IBurgerMenuProps) => {
+const BurgerMenu = observer(({ visible, setVisible }: IBurgerMenuProps) => {
   return (
-    <Offcanvas show={visible} onHide={() => setVisible(false)}>
+    <Offcanvas show={visible} onHide={() => setVisible(false)} className="w-75">
       <h2>Sidebar</h2>
     </Offcanvas>
   );
-};
+});
 
 export default BurgerMenu;

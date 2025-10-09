57b038d2c20b77b4793ad6b58107cba7f90f81d3
Илья Кузнецов <crmprog3@mtkrosberg.ru>
2025-10-09 10:59:24 +0300

    refactor: remove menu

--- src/components/layuot/Header.tsx
+++ src/components/layuot/Header.tsx
@@ -2,21 +2,21 @@
 // import BurgerMenu from './BurgerMenu';
 // import UserMenu from './UserMenu.tsx';
 
-import BurgerMenu from './BurgerMenu.tsx';
-import { RiMenu2Fill } from 'react-icons/ri';
-import { useState } from 'react';
+// import BurgerMenu from './BurgerMenu.tsx';
+// import { RiMenu2Fill } from 'react-icons/ri';
+// import { useState } from 'react';
 
 const Header = () => {
-  const [isBurgerVisible, setIsBurgerVisible] = useState(false);
+  // const [isBurgerVisible, setIsBurgerVisible] = useState(false);
   // const [isUserMenuVisible, setIsUserMenuVisible] = useState(false);
   return (
     <div
       className="d-flex align-items-center justify-content-between bg-light px-3 w-100"
       style={{ height: '40px' }}
     >
-      <RiMenu2Fill onClick={() => setIsBurgerVisible(true)} role="button" size={25} />
+      {/*<RiMenu2Fill onClick={() => setIsBurgerVisible(true)} role="button" size={25} />*/}
       {/*<BsPersonCircle onClick={() => setIsUserMenuVisible(true)} role="button" size={25} />*/}
-      <BurgerMenu visible={isBurgerVisible} setVisible={setIsBurgerVisible} />
+      {/*<BurgerMenu visible={isBurgerVisible} setVisible={setIsBurgerVisible} />*/}
       {/*<UserMenu visible={isUserMenuVisible} setVisible={setIsUserMenuVisible} />*/}
       {/*<AppBurger visible={isBurgerVisible} setVisible={setIsBurgerVisible} />*/}
     </div>

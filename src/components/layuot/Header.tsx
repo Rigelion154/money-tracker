1afe0b2fac5690539a83688ef028ddb8da07e0d8
Илья Кузнецов <crmprog3@mtkrosberg.ru>
2025-10-14 09:09:37 +0300

    refactor: remove menu

--- src/components/layuot/Header.tsx
+++ src/components/layuot/Header.tsx
@@ -1,25 +1,63 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import { BsPersonCircle } from 'react-icons/bs';
 import { RiMenu2Fill } from 'react-icons/ri';
+import { Button, Dropdown } from 'react-bootstrap';
+import { authStore } from '../../store/AuthStore.ts';
+import { useState } from 'react';
+import BaseLoader from '../helpers/BaseLoader.tsx';
 
-import BurgerMenu from './menu/BurgerMenu.tsx';
-import UserMenu from './menu/UserMenu.tsx';
+// import BurgerMenu from './menu/BurgerMenu.tsx';
+// import UserMenu from './menu/UserMenu.tsx';
 
 const Header = observer(() => {
-  const [isBurgerVisible, setIsBurgerVisible] = useState(false);
-  const [isUserMenuVisible, setIsUserMenuVisible] = useState(false);
+  // const [isBurgerVisible, setIsBurgerVisible] = useState(false);
+  // const [isUserMenuVisible, setIsUserMenuVisible] = useState(false);
+  const { userId } = authStore;
+  const [isLoading, setIsLoading] = useState(false);
+  const handleLogout = async () => {
+    setIsLoading(true);
+    await authStore.logoutUser();
+    setIsLoading(false);
+  };
+
+  if (isLoading) {
+    return (
+        <div className="position-fixed top-0 start-0 w-100 h-100 bg-white" style={{ zIndex: 150 }}>
+          <BaseLoader />
+        </div>
+    );
+  }
 
   return (
     <div
       className="d-flex align-items-center justify-content-between bg-light px-3 w-100"
       style={{ height: '40px' }}
     >
-      <RiMenu2Fill onClick={() => setIsBurgerVisible(true)} role="button" size={25} />
-      <BsPersonCircle onClick={() => setIsUserMenuVisible(true)} role="button" size={25} />
+      <RiMenu2Fill
+          // onClick={() => setIsBurgerVisible(true)}
+          role="button" size={25} />
+      <Dropdown>
+       <Dropdown.Toggle variant='outline-dark' className='border-0 rounded-1'>
+         <BsPersonCircle
+             // onClick={() => setIsUserMenuVisible(true)}
+             role="button" size={25} />
+       </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <div className="d-flex flex-column justify-content-center align-items-center w-100 h-100 gap-3">
+            <h5 className="text-center">{userId}</h5>
+            <Button variant="primary" onClick={handleLogout}>
+              Выход
+            </Button>
+          </div>
+        </Dropdown.Menu>
+      </Dropdown>
 
-      <BurgerMenu visible={isBurgerVisible} setVisible={setIsBurgerVisible} />
-      <UserMenu visible={isUserMenuVisible} setVisible={setIsUserMenuVisible} />
+
+
+
+      {/*<BurgerMenu visible={isBurgerVisible} setVisible={setIsBurgerVisible} />*/}
+      {/*<UserMenu visible={isUserMenuVisible} setVisible={setIsUserMenuVisible} />*/}
     </div>
   );
 });

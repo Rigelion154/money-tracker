3c220134462bbe6551de7f88b5abe5ac5239c817
Илья Кузнецов <crmprog3@mtkrosberg.ru>
2025-10-15 16:36:55 +0300

    refactor: remove percentage

--- src/components/expenses/subcategories/ExpenseSubcategoryDefaultItem.tsx
+++ src/components/expenses/subcategories/ExpenseSubcategoryDefaultItem.tsx
@@ -1,12 +1,9 @@
 import { observer } from 'mobx-react-lite';
 import { useNavigate } from 'react-router-dom';
-import { useMemo } from 'react';
 
 import type { IExpenseCategory, IExpenseSubcategory } from '../../../types/expenses.types.ts';
 
 import { ROUTES } from '../../../routes/routes.ts';
-import { calculatePercentage } from '../../../utils/calculatePersentage.ts';
-import { getCurrencyString } from '../../../utils/getCurrencyString.ts';
 import { modalStore } from '../../../store/ModalStore.ts';
 import { screenStore } from '../../../store/ScreenStore.ts';
 
@@ -20,15 +17,15 @@ const ExpenseSubcategoryDefaultItem = observer(
   ({ category, subcategory }: ISubcategoryItemProps) => {
     const { isMobile } = screenStore;
     const navigate = useNavigate();
-    const percentage = useMemo(
-      () =>
-        calculatePercentage(category.category_total_amount, subcategory.subcategory_total_amount),
-      [category.category_total_amount, subcategory.subcategory_total_amount],
-    );
-    const currencyString = useMemo(
-      () => getCurrencyString(subcategory.subcategory_total_amount),
-      [subcategory.subcategory_total_amount],
-    );
+    // const percentage = useMemo(
+    //   () =>
+    //     calculatePercentage(category.category_total_amount, subcategory.subcategory_total_amount),
+    //   [category.category_total_amount, subcategory.subcategory_total_amount],
+    // );
+    // const currencyString = useMemo(
+    //   () => getCurrencyString(subcategory.subcategory_total_amount),
+    //   [subcategory.subcategory_total_amount],
+    // );
 
     const handleSubcategoryClick = () => {
       if (!isMobile) {
@@ -53,8 +50,13 @@ const ExpenseSubcategoryDefaultItem = observer(
         onClick={handleSubcategoryClick}
       >
         <div className="col-6">{subcategory.title}</div>
-        <div className="col-2 text-center">{percentage}%</div>
-        <div className="col-4 text-end">{currencyString}</div>
+        {/*<div className="col-2 text-center">{percentage}%</div>*/}
+        <div className="col-6 text-end">
+          {subcategory.subcategory_total_amount.toLocaleString('ru-RU', {
+            style: 'currency',
+            currency: 'RUB',
+          })}
+        </div>
       </div>
     );
   },

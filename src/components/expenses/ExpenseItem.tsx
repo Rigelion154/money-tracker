5541cbd014cda121a00349477a8a0948bfbfae54
Илья Кузнецов <crmprog3@mtkrosberg.ru>
2025-10-15 17:43:07 +0300

    refactor: remove acc item

--- src/components/expenses/ExpenseItem.tsx
+++ src/components/expenses/ExpenseItem.tsx
@@ -10,7 +10,6 @@ import { getCurrencyString } from '../../utils/getCurrencyString.ts';
 import ExpenseDetailsModal from './ExpenseDetailsModal.tsx';
 
 import styles from './Expenses.module.css';
-import { Accordion } from 'react-bootstrap';
 
 interface IItemProps {
   date: string;
@@ -28,35 +27,26 @@ const ExpenseItem = observer(({ expenses, date, category, type }: IItemProps) =>
 
   return (
     <div key={date}>
-      <Accordion.Item eventKey={date}>
-        <Divider className="fs__small py-1 text-muted">
-          {moment(date).format('DD MMMM YYYY')}
-        </Divider>
-      </Accordion.Item>
+      <Divider className="fs__small py-1 text-muted">{moment(date).format('DD MMMM YYYY')}</Divider>
 
       {expenses.map((expense) => (
-        <Accordion.Item
-          eventKey={expense.id + expense.category_id}
+        <div
+          className={styles.expense__list_wrapper}
           key={expense.id + expense.category_id}
+          onClick={() => handleExpenseClick(expense.id)}
         >
-          <div
-            className={styles.expense__list_wrapper}
-            key={expense.id + expense.category_id}
-            onClick={() => handleExpenseClick(expense.id)}
-          >
-            {type === 'subcategory' && <span>{category.title}</span>}
+          {type === 'subcategory' && <span>{category.title}</span>}
 
-            {type === 'category' && (
-              <span style={{ color: (category as IExpenseCategory)?.color }}>
-                {(category as IExpenseCategory)?.subcategories?.find(
-                  (subcategory) => subcategory.id === expense.subcategory_id,
-                )?.title ?? category.title}
-              </span>
-            )}
+          {type === 'category' && (
+            <span style={{ color: (category as IExpenseCategory)?.color }}>
+              {(category as IExpenseCategory)?.subcategories?.find(
+                (subcategory) => subcategory.id === expense.subcategory_id,
+              )?.title ?? category.title}
+            </span>
+          )}
 
-            <span className="text-end">{getCurrencyString(expense.amount)}</span>
-          </div>
-        </Accordion.Item>
+          <span className="text-end">{getCurrencyString(expense.amount)}</span>
+        </div>
       ))}
     </div>
   );

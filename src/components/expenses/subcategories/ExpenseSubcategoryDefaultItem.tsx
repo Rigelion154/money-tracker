e8f11fa848ea4b3e527eb84030d79480b6b5bbb6
Илья Кузнецов <crmprog3@mtkrosberg.ru>
2025-10-15 12:44:23 +0300

    refactor: menu

--- src/components/expenses/subcategories/ExpenseSubcategoryDefaultItem.tsx
+++ src/components/expenses/subcategories/ExpenseSubcategoryDefaultItem.tsx
@@ -25,9 +25,9 @@ const ExpenseSubcategoryDefaultItem = ({ category, subcategory }: ISubcategoryIt
   const handleSubcategoryClick = () =>
     modalStore.openModal({ children: <ExpenseSubcategoryModal subcategory={subcategory} /> });
 
-  if (subcategory.expenses.length < 3) {
-    return null;
-  }
+  // if (subcategory.expenses.length < 3) {
+  //   return null;
+  // }
 
   return (
     <div
--- src/components/layuot/Header.tsx
+++ src/components/layuot/Header.tsx
@@ -2,10 +2,8 @@
 import { observer } from 'mobx-react-lite';
 import { BsPersonCircle } from 'react-icons/bs';
 import { RiMenu2Fill } from 'react-icons/ri';
-import { Button, Dropdown } from 'react-bootstrap';
-import { authStore } from '../../store/AuthStore.ts';
+import { Dropdown } from 'react-bootstrap';
 import { useState } from 'react';
-import BaseLoader from '../helpers/BaseLoader.tsx';
 
 // import BurgerMenu from './menu/BurgerMenu.tsx';
 import UserMenu from './menu/UserMenu.tsx';
@@ -13,21 +11,21 @@ import UserMenu from './menu/UserMenu.tsx';
 const Header = observer(() => {
   // const [isBurgerVisible, setIsBurgerVisible] = useState(false);
   const [isUserMenuVisible, setIsUserMenuVisible] = useState(false);
-  const { userId } = authStore;
-  const [isLoading, setIsLoading] = useState(false);
-  const handleLogout = async () => {
-    setIsLoading(true);
-    await authStore.logoutUser();
-    setIsLoading(false);
-  };
+  // const { userId } = authStore;
+  // const [isLoading, setIsLoading] = useState(false);
+  // const handleLogout = async () => {
+  //   setIsLoading(true);
+  //   await authStore.logoutUser();
+  //   setIsLoading(false);
+  // };
 
-  if (isLoading) {
-    return (
-      <div className="position-fixed top-0 start-0 w-100 h-100 bg-white" style={{ zIndex: 150 }}>
-        <BaseLoader />
-      </div>
-    );
-  }
+  // if (isLoading) {
+  //   return (
+  //     <div className="position-fixed top-0 start-0 w-100 h-100 bg-white" style={{ zIndex: 150 }}>
+  //       <BaseLoader />
+  //     </div>
+  //   );
+  // }
 
   return (
     <div
@@ -43,19 +41,19 @@ const Header = observer(() => {
         </Dropdown.Menu>
       </Dropdown>
 
-      <Dropdown drop="start">
-        <Dropdown.Toggle variant="outline-dark" className="border-0 rounded-1">
-          <BsPersonCircle size={25} />
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          <div className="d-flex flex-column justify-content-center align-items-center w-100 h-100 gap-3">
-            <h5 className="text-center">{userId}</h5>
-            <Button variant="primary" onClick={handleLogout}>
-              Выход
-            </Button>
-          </div>
-        </Dropdown.Menu>
-      </Dropdown>
+      {/*<Dropdown drop="start">*/}
+      {/*  <Dropdown.Toggle variant="outline-dark" className="border-0 rounded-1">*/}
+      {/*    <BsPersonCircle size={25} />*/}
+      {/*  </Dropdown.Toggle>*/}
+      {/*  <Dropdown.Menu>*/}
+      {/*    <div className="d-flex flex-column justify-content-center align-items-center w-100 h-100 gap-3">*/}
+      {/*      <h5 className="text-center">{userId}</h5>*/}
+      {/*      <Button variant="primary" onClick={handleLogout}>*/}
+      {/*        Выход*/}
+      {/*      </Button>*/}
+      {/*    </div>*/}
+      {/*  </Dropdown.Menu>*/}
+      {/*</Dropdown>*/}
       <BsPersonCircle size={25} onClick={() => setIsUserMenuVisible(true)} />
       {/*<BurgerMenu visible={isBurgerVisible} setVisible={setIsBurgerVisible} />*/}
       <UserMenu visible={isUserMenuVisible} setVisible={setIsUserMenuVisible} />

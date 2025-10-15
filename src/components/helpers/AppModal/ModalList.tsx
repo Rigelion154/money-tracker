49d3fbba9652537f2596383e96eb23b388427ca7
Илья Кузнецов <crmprog3@mtkrosberg.ru>
2025-10-15 14:05:17 +0300

    refactor: styles

--- src/components/helpers/AppModal/ModalList.tsx
+++ src/components/helpers/AppModal/ModalList.tsx
@@ -1,9 +1,14 @@
 import { observer } from 'mobx-react-lite';
 import { modalStore } from '../../../store/ModalStore.ts';
 import AppModal from './AppModal.tsx';
+import { useEffect } from 'react';
 
 const ModalList = observer(function ModalList() {
   const { modalList } = modalStore;
+
+  useEffect(() => {
+    document.body.style.overflowY = modalList.length > 0 ? 'hidden' : 'scroll';
+  }, [modalList.length]);
 
   return (
     <>

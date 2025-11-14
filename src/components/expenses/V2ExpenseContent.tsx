207df1e347352adb39d56e3bec0670b4799b4c26
Илья Кузнецов <crmprog3@mtkrosberg.ru>
2025-11-14 11:24:15 +0300

    fix: category and subcategory check

--- src/components/expenses/V2ExpenseContent.tsx
+++ src/components/expenses/V2ExpenseContent.tsx
@@ -32,42 +32,47 @@ const V2ExpenseContent = observer(() => {
             const category = v2_categories[expenseCategory.categoryId];
 
             return (
-              <Accordion className="col-12 col-md-8 col-xl-4" key={expenseCategory.categoryId}>
-                <Accordion.Button className="border rounded-2 p-2 shadow-sm">
-                  <div className="col-6 px-0 d-flex align-items-center gap-2">
-                    <i
-                      className={`${category.icon} ${styles.expense__icon}`}
+              category && (
+                <Accordion className="col-12 col-md-8 col-xl-4" key={expenseCategory.categoryId}>
+                  <Accordion.Button className="border rounded-2 p-2 shadow-sm">
+                    <div className="col-6 px-0 d-flex align-items-center gap-2">
+                      <i
+                        className={`${category.icon} ${styles.expense__icon}`}
+                        style={{ backgroundColor: category.color }}
+                      />
+                      <span>{category.title}</span>
+                    </div>
+
+                    <div className="col-2 px-0 text-center">
+                      <small className="fw-bold text-success">{expenseCategory.percentage}%</small>
+                    </div>
+
+                    <div
+                      className="col-4 px-0 text-end overflow-hidden fs__small"
+                      style={{ textOverflow: 'ellipsis' }}
+                    >
+                      <span className="fw-bold text-muted text-end">
+                        {getCurrencyString(expenseCategory.totalAmount)}
+                      </span>
+                    </div>
+                  </Accordion.Button>
+
+                  <Accordion.Body className="border p-1 shadow-sm">
+                    <div
+                      className="text-white rounded-2"
                       style={{ backgroundColor: category.color }}
-                    />
-                    <span>{category.title}</span>
-                  </div>
+                    >
+                      {expenseCategory.subcategories &&
+                        expenseCategory.subcategories.length > 0 &&
+                        expenseCategory.subcategories.map((subcategory) => (
+                          <V2Subcategory {...{ subcategory }} key={subcategory.subcategoryId} />
+                        ))}
+                    </div>
 
-                  <div className="col-2 px-0 text-center">
-                    <small className="fw-bold text-success">{expenseCategory.percentage}%</small>
-                  </div>
-
-                  <div
-                    className="col-4 px-0 text-end overflow-hidden fs__small"
-                    style={{ textOverflow: 'ellipsis' }}
-                  >
-                    <span className="fw-bold text-muted text-end">
-                      {getCurrencyString(expenseCategory.totalAmount)}
-                    </span>
-                  </div>
-                </Accordion.Button>
-
-                <Accordion.Body className="border p-1 shadow-sm">
-                  <div className="text-white rounded-2" style={{ backgroundColor: category.color }}>
-                    {expenseCategory.subcategories &&
-                      expenseCategory.subcategories.length > 0 &&
-                      expenseCategory.subcategories.map((subcategory) => (
-                        <V2Subcategory {...{ subcategory }} key={subcategory.subcategoryId} />
-                      ))}
-                  </div>
-
-                  <V2ExpensesList expenses={expenseCategory.expenses} />
-                </Accordion.Body>
-              </Accordion>
+                    <V2ExpensesList expenses={expenseCategory.expenses} />
+                  </Accordion.Body>
+                </Accordion>
+              )
             );
           })}
         </div>
--- src/components/expenses/V2Subcategory.tsx
+++ src/components/expenses/V2Subcategory.tsx
@@ -1,15 +1,13 @@
+import { Accordion } from 'react-bootstrap';
 import { observer } from 'mobx-react-lite';
 
 import type { IV2ExpenseSubcategory } from '../../types/expenses.types.ts';
 
-// import { modalStore } from '../../store/ModalStore.ts';
+import { authStore } from '../../store/AuthStore.ts';
 import { subcategoriesStore } from '../../store/SubcategoriesStore.ts';
 import { getCurrencyString } from '../../utils/getCurrencyString.ts';
 
-// import ExpenseSubcategoryModal from './ExpenseSubcategoryModal.tsx';
-import { Accordion } from 'react-bootstrap';
 import V2ExpensesList from './V2ExpensesList.tsx';
-import { authStore } from '../../store/AuthStore.ts';
 
 interface ISubcategoryProps {
   subcategory: IV2ExpenseSubcategory;
@@ -24,16 +22,18 @@ const V2Subcategory = observer(({ subcategory }: ISubcategoryProps) => {
   }
 
   return (
-    <Accordion>
-      <Accordion.Button className="py-1 px-2 bg-transparent shadow-none text-white">
-        <div className="col-6">{subcategories?.[subcategory.subcategoryId].title}</div>
-        <div className="col-2 text-center">{subcategory.percentage}%</div>
-        <div className="col-4 text-end">{getCurrencyString(subcategory.totalAmount)}</div>
-      </Accordion.Button>
-      <Accordion.Body className="bg-light border text-dark px-0 py-1">
-        <V2ExpensesList expenses={subcategory.expenses} />
-      </Accordion.Body>
-    </Accordion>
+    subcategories && (
+      <Accordion>
+        <Accordion.Button className="py-1 px-2 bg-transparent shadow-none text-white">
+          <div className="col-6">{subcategories?.[subcategory.subcategoryId]?.title}</div>
+          <div className="col-2 text-center">{subcategory.percentage}%</div>
+          <div className="col-4 text-end">{getCurrencyString(subcategory.totalAmount)}</div>
+        </Accordion.Button>
+        <Accordion.Body className="bg-light border text-dark px-0 py-1">
+          <V2ExpensesList expenses={subcategory.expenses} />
+        </Accordion.Body>
+      </Accordion>
+    )
   );
 });
 
